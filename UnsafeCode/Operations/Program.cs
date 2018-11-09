2ee179296536815151e5b690ea83a27f80671f86
Oleksii Matsapura <godhedin2000@gmail.com>
2018-11-09 13:41:01 +0200

    Added Operations

---
+++ UnsafeCode/Operations/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                int* x;
+                int y = 10;
+
+                x = &y;
+
+                Console.WriteLine(*x);
+
+                y = y + 20;
+                Console.WriteLine(*x);
+
+                *x = 50;
+
+                Console.WriteLine(y);            
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
