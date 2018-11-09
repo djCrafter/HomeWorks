fc934e0d40178f02f23ba8981014786a9528cb68
Oleksii Matsapura <godhedin2000@gmail.com>
2018-11-09 13:26:33 +0200

    Added UnsafeCode

---
+++ UnsafeCode/UnsafeCode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnsafeCode
+{
+    class Program
+    {
+        unsafe struct State
+        {
+
+        }
+
+        unsafe private static void PointerMethod()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+
+            }
+        }
+    }
+}
