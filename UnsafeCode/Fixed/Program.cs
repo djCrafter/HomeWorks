1a2cb415e7abe89e477ab6903afbc276f1f2ef26
Oleksii Matsapura <godhedin2000@gmail.com>
2018-11-09 15:34:00 +0200

    Added Fixed

---
+++ UnsafeCode/Fixed/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fixed
+{
+    public class Person
+    {
+        public int age;
+        public int height;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unsafe
+            {
+                Person person = new Person();
+                person.age = 28;
+                person.height = 178;
+
+                fixed (int* p = &person.age)
+                {
+                    if(*p < 30)
+                    {
+                        *p = 30;
+                    }
+                }
+
+                Console.WriteLine(person.age);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
