1d65760192057335a266c8ffa8b1b78f63565c27
Oleksii Matsapura <godhedin2000@gmail.com>
2018-11-22 15:14:55 +0200

    Add ParamethrizedThreadStartExample

---
+++ ParametrizedThreadStartExample/ParametrizedThreadStartExample/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ParametrizedThreadStartExample
+{
+
+    class Program
+    {
+        public static void Encryption(object obj)
+        {
+            while (true)
+            {
+                
+               
+            }
+        }
+
+        public static void Decryption(object obj)
+        {
+            while (true)
+            {
+                
+              
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            ParameterizedThreadStart Param = null;
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("1.Шифровать.");
+                Console.WriteLine("2.Дешифровать.");
+
+                ConsoleKeyInfo Select = Console.ReadKey(true);
+                Console.Clear();
+
+                if(ConsoleKey.D1 == Select.Key)
+                {
+                    Param = new ParameterizedThreadStart(Encryption);
+                    Console.WriteLine("Введите путь к файлу который хотите зашифровать!");
+                }
+                else if(ConsoleKey.D2 == Select.Key)
+                {
+                    Param = new ParameterizedThreadStart(Decryption);
+                    Console.WriteLine("Введите путь к файлу который хотите расшифровать!");
+                }
+
+                if(ConsoleKey.D1 == Select.Key || ConsoleKey.D2 == Select.Key)
+                {
+                    string FilePath = Console.ReadLine();
+
+                    Thread thread = new Thread(Param);
+                    thread.Start((object)FilePath);
+                    Console.WriteLine("Нажмите символ чтобы выполнить действие!");
+
+                    do
+                    {
+                        Console.WriteLine("[c] Отменить работу потока");
+                        Console.WriteLine("[p] приостановить или возобновить работу потока");
+
+                        ConsoleKeyInfo Selects = Console.ReadKey(true);
+
+                        if (Selects.Key == ConsoleKey.C)
+                        {
+                            if (thread.ThreadState == ThreadState.Running)
+                            {
+                                thread.Abort();
+
+                                Console.WriteLine("Поток остановлен!");
+                            }
+                        }
+                        else if (Selects.Key == ConsoleKey.P)
+                        {
+                            if (thread.ThreadState == ThreadState.Running)
+                            {
+                                thread.Suspend();
+                                Console.WriteLine("Поток приостановлен!");
+                            }
+                            else if (thread.ThreadState == ThreadState.Suspended)
+                            {
+                                thread.Resume();
+                                Console.WriteLine("Поток восстановли работу!");
+                            }
+                            Thread.Sleep(100);
+                        }
+                    } while (thread.ThreadState == ThreadState.Suspended || thread.ThreadState == ThreadState.Running);
+                    Console.ReadKey(true);
+                    Console.Clear();
+                }
+
+
+            }
+        }
+    }
+}
---
+++ ParametrizedThreadStartExample/ParametrizedThreadStartExample2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ParametrizedThreadStartExample2
+{
+    class Program
+    {
+        static void LisenerClient()
+        {
+            int Counter = 0;
+
+            while (true)
+            {
+                Console.WriteLine("Нажмите любую клавишу для симуляции подключения!!!");
+                Console.ReadKey(true);
+                ParameterizedThreadStart UserDel = new ParameterizedThreadStart(UserThreadFunk);
+
+                Thread UserWorkThread = new Thread(UserDel);
+
+                UserWorkThread.Start((object)Counter.ToString());
+                Counter++;
+
+            }
+        }
+
+        static void UserThreadFunk(object a)
+        {
+            string UserName = (string)a;
+
+            Console.WriteLine("пользователь\n# " + UserName + "подключился");
+
+            while (true)
+            {
+                switch (GetUserCommand())
+                {
+                    case 0:
+                        Console.WriteLine("# {0} подписался на новости", UserName);
+                        break;
+                    case 1:
+                        Console.WriteLine("# {0} начал чат", UserName);
+                        break;
+                    case 2:
+                        Console.WriteLine("# {0} купил продукцию в магазине", UserName);
+                        break;
+                    case 3:
+                        Console.WriteLine("# {0} отправил письмо", UserName);
+                        break;
+                    case 4:
+                        Console.WriteLine("# {0} отключился", UserName);
+                        return;
+                }
+                Thread.Sleep(2000);
+            }
+        }
+
+        static int GetUserCommand()
+        {
+            Random random = new Random();
+
+            return random.Next(0, 5);
+
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadStart Lis = new ThreadStart(LisenerClient);
+            Thread LisenerThread = new Thread(Lis);
+            LisenerThread.IsBackground = false;
+            LisenerThread.Start();
+        }
+    }
+}
