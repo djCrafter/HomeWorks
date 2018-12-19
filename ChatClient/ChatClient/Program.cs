70dd756033847adc16a506663eaa8f093d2a2e15
Oleksii Matsapura <godhedin2000@gmail.com>
2018-12-19 20:47:45 +0200

    Added ChatServer & ChatClinet

---
+++ ChatClient/ChatClient/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static string userName;
+        private const string host = "127.0.0.1";
+        private const int port = 8888;
+        static TcpClient client;
+        static NetworkStream stream;
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            userName = Console.ReadLine();
+            client = new TcpClient();
+            try
+            {
+                client.Connect(host, port);
+                stream = client.GetStream();
+
+                string message = userName;
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                Thread reciveThread = new Thread(new ThreadStart(ReciveMessage));
+                reciveThread.Start();
+                Console.WriteLine("Добро пожаловать, {0} ", userName);
+                SendMessage();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Disconnect();
+            }
+
+        }
+
+        static void SendMessage()
+        {
+            Console.WriteLine("Введите сообщение: ");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+        }
+
+        static void ReciveMessage()
+        {
+            while (true)
+            {
+                try
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    Console.WriteLine(message);
+                }
+                catch
+                {
+                    Console.WriteLine("Подключение прервано!!!");
+                    Console.ReadLine();
+                    Disconnect();
+                }
+            }
+        }
+
+        static void Disconnect()
+        {
+            if (stream != null)
+                stream.Close();
+            if (client != null)
+                client.Close();
+            Environment.Exit(0);
+        }
+    }
+}
